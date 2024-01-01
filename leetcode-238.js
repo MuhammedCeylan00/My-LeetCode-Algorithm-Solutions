e10106f17fdf64d9283697701eddbfe0cde8d4d0
Muhammed ceylan <ceylanmuhammed18.81@gmail.com>
2024-01-01 14:17:22 +0300

    First 2024 commit.

--- leetcode-238.js
+++ leetcode-238.js
@@ -4,7 +4,7 @@
 
 // You must write an algorithm that runs in O(n) time and without using the division operation.
 
- 
+
 
 // Example 1:
 
@@ -16,13 +16,31 @@
 // Output: [0,0,9,0,0]
 
 
-var productExceptSelf = function(nums) {
-    let myArray = []
-    let multiplication = 1;
-    for (let index = 0; index < nums.length; index++) {
-        const element = nums[index];
-        multiplication = multiplication * element
+var productExceptSelf = function (nums) {
+    let prefix = []
+    for (let i = 0; i < nums.length; i++) {
+        if (i == 0) {
+            prefix[i] = 1
+        } else {
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+        }
     }
-    console.log(multiplication/-1);
+
+    let suffix = [];
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (i == nums.length - 1) {
+            suffix[i] = 1
+        } else {
+            suffix[i] = suffix[i + 1] * nums[i + 1]
+        }
+    }
+
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = prefix[i]*suffix[i];
+    }
+
+    return result;
+
 };
-productExceptSelf([-1,1,0,-3,3])
+console.log(productExceptSelf([1,2,3,4]))
